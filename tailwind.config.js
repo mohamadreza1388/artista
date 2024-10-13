1dce4943515eb70917af4bbe7cb3fda39ccbb2c3
mohamadreza <mohamadreza1388.org@gmail.com>
2024-10-14 00:27:53 +0330

    feat(header)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        extend: {
+            backgroundImage: {
+                'body': "linear-gradient(210deg, #E1E7FF 18.31%, #708DFA 155.04%)",
+            }
+        }
+    },
+    plugins: [],
+}
